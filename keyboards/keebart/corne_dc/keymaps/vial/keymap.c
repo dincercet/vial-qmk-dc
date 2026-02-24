6389bf64949aadb0bc182987d61ea8ddc59e127f
dindindindindin <dincercetiner@hotmail.com>
2026-02-24 12:22:03 +0300

    eng-base alpha-2

--- keyboards/keebart/corne_dc/keymaps/vial/keymap.c
+++ keyboards/keebart/corne_dc/keymaps/vial/keymap.c
@@ -50,6 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        '*', 'L', 'L', 'L', 'L', 'L', '*',  '*', 'R', 'R', 'R', 'R', 'R', '*',
+        '*', 'L', 'L', 'L', 'L', 'L', '*',  '*', 'R', 'R', 'R', 'R', 'R', '*',
+        '*', 'L', 'L', 'L', 'L', 'L',            'R', 'R', 'R', 'R', 'R', '*',
+                            '*', '*', '*',  '*', '*', '*'
+    );
+
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =
